421db150fe2222b250af820ffb860e6694fd3f40
SamirAhma <ahmaddin1000@gmail.com>
2020-07-17 14:43:06 +0800

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,12 +34,7 @@ const IndexPage = () => (
     <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
       {/* You still need to add the hidden input with the form name to your JSX form */}
       <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
+
       <p>
         <label>Message: <textarea name="message"></textarea></label>
       </p>
