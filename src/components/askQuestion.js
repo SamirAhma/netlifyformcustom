bc9d6e38b113b0e0c8c3a9c998a83981efb2709f
SamirAhma <ahmaddin1000@gmail.com>
2020-07-19 10:52:40 +0800

    asynchronos ask question

--- src/components/askQuestion.js
+++ src/components/askQuestion.js
@@ -56,7 +56,7 @@ class AskQuestion extends React.Component {
 
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       data: qs.stringify(formData),
-      url: "https://laughing-pike-8aff9d.netlify.app"
+      url: "https://nostalgic-spence-60f3f1.netlify.app"
     }
 
     // Submit to Netlify. Upon success, set the feedback message and clear all
